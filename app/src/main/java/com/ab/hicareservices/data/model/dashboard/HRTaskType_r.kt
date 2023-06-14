ddbb3eee94a12e3b7e678249bfe649524224a719
akshay <akshay.tabib@hicare.in>
2023-06-14 20:07:08 +0530

    Backpress issue solve and getdashboard add in cod and today and upcomming model class add

---
+++ app/src/main/java/com/ab/hicareservices/data/model/dashboard/HRTaskType_r.kt
@@ -0,0 +1,10 @@
+package com.ab.hicareservices.data.model.dashboard
+
+import com.google.gson.annotations.SerializedName
+
+data class HRTaskType_r(
+    @SerializedName("Id") var Id: String? = null,
+    @SerializedName("Name") var Name: String? = null
+) {
+
+}
