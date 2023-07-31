7977ec1bbfc01fc72f7e95b7120d0b3316cddc0d
akshay <akshay.tabib@hicare.in>
2023-07-31 19:02:14 +0530

    changes in notification.

--- app/src/main/java/com/ab/hicareservices/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/ab/hicareservices/utils/MyFirebaseMessagingService.kt
@@ -73,55 +73,60 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         tag: String?,
         data: MutableMap<String, String>
     ) {
-        val intent = Intent(this, HomeActivity::class.java)
-        // Assign channel ID
-        val channel_id = channelId
-        // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
-        // the activities present in the activity stack,
-        // on the top of the Activity that is to be launched
+        try {
 
 
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        // Pass the intent to PendingIntent to start the
-        // next Activity
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent,
-            PendingIntent.FLAG_MUTABLE
-        )
-
-        // Create a Builder object using NotificationCompat
-        // class. This will allow control over all the flags
-
-        val remoteViews = RemoteViews("com.ab.hicareservices", R.layout.layout_notification)
-
-        var builder: NotificationCompat.Builder =
-            NotificationCompat.Builder(applicationContext, channel_id.toString())
-                .setSmallIcon(R.drawable.reviewdialog_round_icon)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
-                .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
-                .setContentIntent(pendingIntent)
-                .setContent(remoteViews)
-
-        remoteViews.setTextViewText(R.id.notificationtitle, title)
-        remoteViews.setTextViewText(R.id.notificationtext, message)
-        remoteViews.setImageViewResource(R.id.image, R.drawable.hicarelogo)
+            val intent = Intent(this, HomeActivity::class.java)
+            // Assign channel ID
+            val channel_id = channelId
+            // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
+            // the activities present in the activity stack,
+            // on the top of the Activity that is to be launched
 
 
-        builder = builder.setCustomContentView(
-            getCustomDesign(title, message)
-        )
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?        // Check if the Android Version is greater than Oreo
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                channel_id,
-                "HicareServices",
-                NotificationManager.IMPORTANCE_HIGH
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            // Pass the intent to PendingIntent to start the
+            // next Activity
+            val pendingIntent = PendingIntent.getActivity(
+                this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT
             )
-            notificationManager?.createNotificationChannel(notificationChannel)
+
+            // Create a Builder object using NotificationCompat
+            // class. This will allow control over all the flags
+
+            val remoteViews = RemoteViews("com.ab.hicareservices", R.layout.layout_notification)
+
+            var builder: NotificationCompat.Builder =
+                NotificationCompat.Builder(applicationContext, channel_id.toString())
+                    .setSmallIcon(R.drawable.reviewdialog_round_icon)
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true)
+                    .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
+                    .setContentIntent(pendingIntent)
+                    .setContent(remoteViews)
+
+            remoteViews.setTextViewText(R.id.notificationtitle, title)
+            remoteViews.setTextViewText(R.id.notificationtext, message)
+            remoteViews.setImageViewResource(R.id.image, R.drawable.hicarelogo)
+
+
+            builder = builder.setCustomContentView(
+                getCustomDesign(title, message)
+            )
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?        // Check if the Android Version is greater than Oreo
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationChannel = NotificationChannel(
+                    channel_id,
+                    "HicareServices",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                notificationManager?.createNotificationChannel(notificationChannel)
+            }
+            notificationManager?.notify(0, builder.build())
+        }catch (e:Exception){
+
         }
-        notificationManager?.notify(0, builder.build())
     }
 }
